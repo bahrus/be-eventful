f939b0a10e4f0099437e0ea5ad32f021c73711c8
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-02-18 19:44:46 -0500

    Initial commit

--- be-eventful.ts
+++ be-eventful.ts
@@ -15,7 +15,11 @@ export class BeEventful extends EventTarget implements Actions {
 //const isoDateExpression = /(?<year>[0-9]{4})-(?<month>[0-9]{2})-(?<day>[0-9]{2})/;
 const reCamelConfigShortKey = /on(?<eventName>[A-Z][a-z]+)$/;
 const reCamelConfigMediumKey = /on(?<eventName>[A-Z][a-z]+)Of(?<camelQry>[A-Z][a-z]+)Do/;
-const reCamelConfigLargeKey = /on(?<eventName>[A-Z][a-z]+)Of(?<camelQry>[A-Z][a-z]+)Do(?<action>[A-Z][a-z]+)/;
+const reCamelConfigLongKey = /on(?<eventName>[A-Z][a-z]+)Of(?<camelQry>[A-Z][a-z]+)Do(?<action>[A-Z][a-z]+)/;
+
+const reCamelConfigEventSubscriptShortKey = /(?<eventName>[A-Z][a-z]+)$/;
+const reCamelConfigEventSubscriptMediumKey = /(?<eventName>[A-Z][a-z]+)Of(?<camelQry>[A-Z][a-z]+)Do/;
+const reCamelConfigEventSubscriptLongKey = /(?<eventName>[A-Z][a-z]+)Of(?<camelQry>[A-Z][a-z]+)Do(?<action>[A-Z][a-z]+)/;
 
 const tagName = 'be-eventful';
 const ifWantsToBe = 'eventful';
